29794934cbfc790a1e399c403c8e1625102e0136
Matthew Sinclair <matthew.sinclair@xero.com>
2023-08-17 15:32:11 +1200

    Revert "fix score calculation logic for reverse order"
    This reverts commit 90d612cfae8e0b9390a6c968571f358c337efff9.

--- logic/calculateHappinessScore.js
+++ logic/calculateHappinessScore.js
@@ -1,4 +1,4 @@
 function calculateHappinessScore(idx, numberOfOptions) {
   const lastOptionIndex = numberOfOptions - 1;
-  return 100 - Math.floor((idx / lastOptionIndex) * 100);
+  return Math.floor((idx / lastOptionIndex) * 100);
 }
